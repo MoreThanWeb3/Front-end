56022950edcbfddfb7acf5f83b05b2a1a79922cf
Tiavina19 <hei.tiavina@gmail.com>
2024-05-30 14:24:58 +0300

    feat: update section cars

--- src/app/components/Brands.js
+++ src/app/components/Brands.js
@@ -1,3 +1,68 @@
+"use client";
+
+// image
+import Image from "next/image";
+
+// framer
+import { motion } from "framer-motion";
+
+// variants
+import { fadeIn } from "/variants";
+
 export default function Brands() {
-  return <div>Brands</div>;
+  return (
+    <section className="xl:pt-16 xl:h-{200px} bg-white flex flex-col justify-center">
+      <div className="container mx-auto">
+        {/* brand wrapper */}
+        <div className="grid grid-cols-3 gap-6 place-items-center xl:flex xl:flex-wrap xl:gap-6 xl:justify-between">
+          <div>
+            <Image
+              src={"icons/brands/ford.svg"}
+              width={85}
+              height={32}
+              alt=""
+            />
+          </div>
+          <div>
+            <Image
+              src={"icons/brands/mercedes.svg"}
+              width={60}
+              height={60}
+              alt=""
+            />
+          </div>
+          <div>
+            <Image
+              src={"icons/brands/audi.svg"}
+              width={85}
+              height={50}
+              alt=""
+            />
+          </div>
+          <div>
+            <Image src={"icons/brands/bmw.svg"} width={60} height={60} alt="" />
+          </div>
+          <div>
+            <Image src={"icons/brands/vw.svg"} width={60} height={60} alt="" />
+          </div>
+          <div>
+            <Image
+              src={"icons/brands/skoda.svg"}
+              width={60}
+              height={60}
+              alt=""
+            />
+          </div>
+          <div>
+            <Image
+              src={"icons/brands/mazda.svg"}
+              width={62}
+              height={50}
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
