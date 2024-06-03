e707c99869139477702b98e3b325b7e94bcd45d4
Tiavina19 <hei.tiavina@gmail.com>
2024-06-03 11:39:52 +0300

    feat: add Why.js and update about.js

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -4,10 +4,10 @@
 import Image from "next/image";
 
 //icons
-import { 
-  MdOutlineMapsHomeWork, 
-  MdOutlineBuildCircle, 
-  MdOutlineDirectionsCar
+import {
+  MdOutlineMapsHomeWork,
+  MdOutlineBuildCircle,
+  MdOutlineDirectionsCar,
 } from "react-icons/md";
 
 // react count up
@@ -25,7 +25,7 @@ import { fadeIn } from "/variants";
 const About = () => {
   const [ref, inView] = useInView({
     threshold: 0.5,
-});
+  });
   return (
     <section className="section flex items-center" id="about" ref={ref}>
       <div className="containeer mx-auto">
@@ -35,7 +35,7 @@ const About = () => {
             variants={fadeIn("up", 0.2)}
             initial="hidden"
             whileInView={"show"}
-            viewport={{ once: false, amount: 0.6}}
+            viewport={{ once: false, amount: 0.6 }}
             className="flex-1 mb-8 xl:mb-0"
           >
             <Image
@@ -48,21 +48,21 @@ const About = () => {
           </motion.div>
           {/* text & stats */}
           <div className="flex-1 flex items-center xl:justify-center">
-            <div className="xl:max-w-[700px]">
-              <motion.h2 
+            <div className="xl:max-w-[517px]">
+              <motion.h2
                 variants={fadeIn("up", 0.4)}
                 initial="hidden"
                 whileInView={"show"}
-                viewport={{ once: false, amount: 0.6}}
-                className="xl:max-w-[517px]"
+                viewport={{ once: false, amount: 0.6 }}
+                className="h2"
               >
                 Car services simplidied.
               </motion.h2>
-              <motion.p 
+              <motion.p
                 variants={fadeIn("up", 0.6)}
                 initial="hidden"
                 whileInView={"show"}
-                viewport={{ once: false, amount: 0.6}}
+                viewport={{ once: false, amount: 0.6 }}
                 className="mb-[42px] max-w-md"
               >
                 Rent, choose, and repair with ease. Our convenient locations,
@@ -70,16 +70,17 @@ const About = () => {
                 car experience.
               </motion.p>
               {/* stats */}
-              <motion.div 
+              <motion.div
                 variants={fadeIn("up", 0.8)}
                 initial="hidden"
                 whileInView={"show"}
-                viewport={{ once: false, amount: 0.2}}
-                className="flex items-center gap-x-8 mb-12">
+                viewport={{ once: false, amount: 0.2 }}
+                className="flex items-center gap-x-8 mb-12"
+              >
                 {/* car types */}
                 <div className="flex flex-col w-[100px]">
                   <MdOutlineDirectionsCar className="text-5xl text-accent mb-2" />
-                  <div>
+                  <div className="text-3xl font-black mb-2">
                     {inView ? (
                       <CountUp start={0} end={50} duration={3} delay={1} />
                     ) : null}
@@ -92,24 +93,23 @@ const About = () => {
                 {/* rental outlets */}
                 <div className="flex flex-col w-[100px]">
                   <MdOutlineMapsHomeWork className="text-5xl text-accent mb-2" />
-                  <div>
+                  <div className="text-3xl font-black mb-2">
                     {inView ? (
                       <CountUp start={0} end={135} duration={3} delay={1} />
                     ) : null}
                     +
                   </div>
                   <div className="uppercase text-[13px] font-semibold text-secondary">
-                    rental <br /> outlets 
+                    rental <br /> outlets
                   </div>
                 </div>
                 {/* repair points */}
                 <div className="flex flex-col w-[100px]">
                   <MdOutlineBuildCircle className="text-5xl text-accent mb-2" />
-                  <div>
+                  <div className="text-3xl font-black mb-2">
                     {inView ? (
                       <CountUp start={0} end={35} duration={3} delay={1} />
                     ) : null}
-                    
                   </div>
                   <div className="uppercase text-[13px] font-semibold text-secondary">
                     repair <br /> points
@@ -117,17 +117,17 @@ const About = () => {
                 </div>
                 {/* btn */}
               </motion.div>
-                <motion.button 
-                  variants={fadeIn("up", 1)}
-                  initial="hidden"
-                  whileInView={"show"}
-                  viewport={{ once: false, amount: 0.6}}
-                  className="hidden xl:block bg-accent hover:bg-accent-hover
+              <motion.button
+                variants={fadeIn("up", 1)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.6 }}
+                className="hidden xl:block bg-accent hover:bg-accent-hover
                 rounded-[10px] w-full h-16 uppercase font-medium text-white tracking-
                 [2px] text-[13px] max-w-[184px]"
-                >
-                  See all cars
-                </motion.button>
+              >
+                See all cars
+              </motion.button>
             </div>
           </div>
         </div>
