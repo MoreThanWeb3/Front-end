183eea5eccf5c1a1473efb919311de0677cbe339
Ravo1401 <hei.ravo@gmail.com>
2024-06-05 07:42:21 +0200

    feat: update contact section

--- src/app/components/Cta.js
+++ src/app/components/Cta.js
@@ -14,19 +14,34 @@ export default function Cta() {
     [#b2b7c2]/10"
       id="contact"
     >
-      <div className="container.mx-auto">
-        <div>
-          {}
-          <div>
-            <div>
-              <h2>Download our App now and hit the road with ease</h2>
-              <p>LOREM IPSUM</p>
-              {}
+      <div className="container mx-auto">
+        <div className="flex flex-col md:flex-row xl:items-center">
+          {/* text */}
+          <div className="flex-1 xl:ml-24 text-center md:text-left mb-12 md:mb-0">
+            <div className="max-w-[520px] mx-auto order-2 xl:order-none">
+              <motion.h2 
+                variants={fadeIn("right", 0.2)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.6 }}
+                className="h2">
+                Download our App now and hit the road with ease
+              </motion.h2>
+              <motion.p
+                variants={fadeIn("right", 0.4)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.6 }}
+                className="mb-10"
+              >
+                LOREM IPSUM
+              </motion.p>
+              {/* btns */}
               <motion.div
-                 variants={fadeIn("up", 0.6)}
-                 initial="hidden"
-                 whileInView={"show"}
-                 viewport={{ once: false, amount: 0.6 }}
+                variants={fadeIn("right", 0.6)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.6 }}
                 className="flex gap-x-3 justify-center md:justify-start">
                 <button className="btn-cta">
                   <Image
@@ -38,7 +53,7 @@ export default function Cta() {
                 </button>
                 <button className="btn-cta">
                   <Image
-                    src={"/icons/buttons/google-play.svg"}
+                    src={"/icons/buttons/app-store.svg"}
                     width={132}
                     height={36}
                     alt=""
